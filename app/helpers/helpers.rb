d5368bf3dbe8367712e38874dff84f12f237ca33
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-12-27 02:09:32 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,11 @@
 class Helpers
 
   def current_user(sessions)
+    @user = User.find_by(username: params[:username])
+    if @user
+      sessions[:user_id] = @user.id
+      redirect '/account'
+    end
   end
 
   def is_logged_in?
